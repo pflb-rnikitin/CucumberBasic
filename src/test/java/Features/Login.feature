3dfc6bf9130b9055f40ea0ac4ed670703cbd4d87
bashoku <r.nikitin@pflb.ru>
2017-07-06 00:17:55 +0400

    cucumber basics explained

--- src/test/java/Features/Login.feature
+++ src/test/java/Features/Login.feature
@@ -3,8 +3,20 @@ Feature: LoginFeature
 
   Scenario: Login with correct username and password
     Given I navigate to the login page
+    And I enter the users email address as email:admin
     And I enter the following authentication credentials
-      | Username  | Password    |
+      | username  | password    |
       | v.m.varga | Tasmania008 |
     And I click login button
     Then I should see the inbox page
+
+  Scenario Outline: Login with correct username and password using scenario outline
+    Given I navigate to the login page
+    And I enter the <username> and <password>
+    And I click login button
+    Then I should see the inbox page
+
+    Examples:
+      | username  | password    |
+      | v.m.varga | Tasmania008 |
+      | wrongname | wrongpass   |
--- src/test/java/Steps/LoginStep.java
+++ src/test/java/Steps/LoginStep.java
@@ -1,16 +1,27 @@
 package Steps;
 
+import Base.BaseUtil;
+import Transformation.EmailTransform;
 import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.Transform;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Nikitin Ruslan on 05.07.2017.
  */
-public class LoginStep {
+public class LoginStep extends BaseUtil {
+    private BaseUtil base;
+
+    public LoginStep(BaseUtil base) {
+        this.base = base;
+    }
+
     @Given("^I navigate to the login page$")
     public void iNavigateToTheLoginPage() throws Throwable {
         System.out.println("Navigate to the login page");
@@ -21,7 +32,30 @@ public class LoginStep {
         List<List<String>> data = table.raw();
         System.out.println(data.get(0).get(0).toString());
         System.out.println(data.get(0).get(1).toString());
+        List<User> users = new ArrayList<User>();
+        users = table.asList(User.class);
+        for (User user: users){
+            System.out.println("The username is" + user.username);
+            System.out.println("The username is" + user.password);
+        }
     }
+
+    @And("^I enter the ([^\"]*) and ([^\"]*)$")
+    public void iEnterTheUserameAndPassword(String username, String password) throws Throwable {
+        System.out.println("Username is" + username);
+        System.out.println("Password is" + password);
+    }
+
+    public class User {
+        public String username;
+        public String password;
+
+        public User(String userName, String passWord) {
+            username = userName;
+            password = passWord;
+        }
+    }
+
 
     @And("^I click login button$")
     public void iClickLoginButton() throws Throwable {
@@ -31,5 +65,11 @@ public class LoginStep {
     @Then("^I should see the inbox page$")
     public void iShouldSeeTheInboxPage() throws Throwable {
         System.out.println("I should see the inbox page");
+        System.out.println("The driver is " + base.stepInfo);
+    }
+
+    @And("^I enter the users email address as email:([^\"]*)$")
+    public void iEnterTheUsersEmailAddressAsEmailAdmin(@Transform(EmailTransform.class) String email) throws Throwable {
+        System.out.println("The email address is " + email);
     }
 }
